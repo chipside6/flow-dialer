ccc973db56581094ef2c308a3167cfedd3d3fa2d
chipside6 <cchips474@gmail.com>
2025-03-22 19:41:51 -0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const subscriptionRoutes = require('./routes/subscriptions');
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
